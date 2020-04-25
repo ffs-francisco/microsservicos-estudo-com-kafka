30a3271e5b7a00b0608396c9b9a9bfc522150890
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-04-25 20:36:47 -0300

    Fixes name classes

--- Cosumer de Pedidos/src/main/java/com/ffs/consumer/ConsumerPedidoApplication.java
+++ Cosumer de Pedidos/src/main/java/com/ffs/consumer/ConsumerPedidoApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ConsumirPedidoApplication {
+public class ConsumerPedidoApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ConsumirPedidoApplication.class, args);
+        SpringApplication.run(ConsumerPedidoApplication.class, args);
     }
 }
--- Cosumer de Pedidos/src/test/java/com/ffs/consumer/ConsumerPedidoApplicationTests.java
+++ Cosumer de Pedidos/src/test/java/com/ffs/consumer/ConsumerPedidoApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ConsumirPedidoApplicationTests {
+class ConsumerPedidoApplicationTests {
 
     @Test
     void contextLoads() {
