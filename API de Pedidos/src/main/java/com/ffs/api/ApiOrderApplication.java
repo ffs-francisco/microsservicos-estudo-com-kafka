7db970d14fd456c29f7d483d47d7e516b24c3bf3
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-04-25 14:56:31 -0300

    Improving class names

--- API de Pedidos/src/main/java/com/ffs/api/ApiOrderApplication.java
+++ API de Pedidos/src/main/java/com/ffs/api/ApiOrderApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiDePedidodosEstudoKafkaApplication {
+public class ApiOrderApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ApiDePedidodosEstudoKafkaApplication.class, args);
+        SpringApplication.run(ApiOrderApplication.class, args);
     }
 }
