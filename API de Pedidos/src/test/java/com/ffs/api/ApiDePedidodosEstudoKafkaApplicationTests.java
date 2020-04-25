4c4c624bdb173e103f3438c1a1a6851e5ab38b64
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-04-25 14:34:31 -0300

    Adding order API MAVEN project

---
+++ API de Pedidos/src/test/java/com/ffs/api/ApiDePedidodosEstudoKafkaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.ffs.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDePedidodosEstudoKafkaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
