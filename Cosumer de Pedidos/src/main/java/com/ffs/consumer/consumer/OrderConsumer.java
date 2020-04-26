8622ed919bd3f797e0527fbb815a831bc4633449
Francisco Silva <fsilvafrancisco16@gmail.com>
2020-04-25 21:03:51 -0300

    Adding plus infos in the log

--- Cosumer de Pedidos/src/main/java/com/ffs/consumer/consumer/OrderConsumer.java
+++ Cosumer de Pedidos/src/main/java/com/ffs/consumer/consumer/OrderConsumer.java
@@ -1,6 +1,7 @@
 package com.ffs.consumer.consumer;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
@@ -13,7 +14,11 @@ import org.springframework.stereotype.Component;
 public class OrderConsumer {
 
     @KafkaListener(topics = "${order.topic-name}", groupId = "${spring.kafka.consumer.group-id}")
-    public void consumer(String order) {
-        log.info("Order : " + order);
+    public void consumer(final ConsumerRecord consumer) {
+
+        log.info("Key       : " + consumer.key());
+        log.info("Headers   : " + consumer.headers());
+        log.info("Partition : " + consumer.partition());
+        log.info("Order     : " + consumer.value() + "\n");
     }
 }
